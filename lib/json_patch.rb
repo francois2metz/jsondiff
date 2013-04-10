db7628e80ba647032327bab08cf939693cc26642
François de Metz <francois@2metz.fr>
2013-04-10 20:47:46 +0200

    Add doc.
    Signed-off-by: François de Metz <francois@stormz.me>

--- lib/json_patch.rb
+++ lib/json_patch.rb
@@ -3,6 +3,12 @@ require 'json_patch/hash_diff'
 require 'json_patch/array_diff'
 
 module JsonPatch
+  # Generate a patch from two ruby hash
+  #
+  # hash1 - the first hash
+  # hash2 - the second hash
+  #
+  # Returns an array of operations
   def self.generate(hash1, hash2)
     HashDiff.generate([], "", hash1, hash2)
   end
