8019d60bc973164cd8cd97b2a63570d93067e37a
François de Metz <francois@2metz.fr>
2013-04-10 21:00:07 +0200

    Reindent all tests.
    Signed-off-by: François de Metz <francois@stormz.me>

--- spec/patch_spec.rb
+++ spec/patch_spec.rb
@@ -8,47 +8,67 @@ describe JsonPatch do
 
     context "add op" do
       it "on hash member" do
-        subject.generate({foo: :bar}, {foo: :bar, baz: :qux}).should == [{ op: :add, path: "/baz", value: :qux }]
+        subject.generate({foo: :bar},
+                         {foo: :bar, baz: :qux})
+          .should == [{ op: :add, path: "/baz", value: :qux }]
       end
 
       it "on array element" do
-        subject.generate({foo: [:bar, :baz]}, {foo: [:bar, :qux, :baz]}).should == [{ op: :add, path: "/foo/1", value: :qux }]
+        subject.generate({foo: [:bar, :baz]},
+                         {foo: [:bar, :qux, :baz]})
+          .should == [{ op: :add, path: "/foo/1", value: :qux }]
       end
 
       it "on nested member object" do
-        subject.generate({foo: :bar}, {foo: :bar, child: {grandchild: {}}}).should == [{ op: :add, path: "/child", value: {grandchild: {}} }]
+        subject.generate({foo: :bar},
+                         {foo: :bar, child: {grandchild: {}}})
+          .should == [{ op: :add, path: "/child", value: {grandchild: {}} }]
       end
 
       it "on more nested member object" do
-        subject.generate({child: {grandchild: {foo: :bar}}}, {child: {grandchild: {foo: :bar, chuck: :norris}}}).should == [{ op: :add, path: "/child/grandchild/chuck", value: :norris }]
+        subject.generate({child: {grandchild: {foo: :bar}}},
+                         {child: {grandchild: {foo: :bar, chuck: :norris}}})
+          .should == [{ op: :add, path: "/child/grandchild/chuck", value: :norris }]
       end
     end
 
     context "remove op" do
       it "on hash member" do
-        subject.generate({foo: :bar, baz: :qux}, {foo: :bar}).should == [{ op: :remove, path: "/baz"}]
+        subject.generate({foo: :bar, baz: :qux},
+                         {foo: :bar})
+          .should == [{ op: :remove, path: "/baz"}]
       end
 
       it "on array element" do
-        subject.generate({foo: [:bar, :qux, :baz]}, {foo: [:bar, :baz]}).should == [{ op: :remove, path: "/foo/1" }]
+        subject.generate({foo: [:bar, :qux, :baz]},
+                         {foo: [:bar, :baz]})
+          .should == [{ op: :remove, path: "/foo/1" }]
       end
     end
 
     context "replace op" do
       it "on hash member" do
-        subject.generate({foo: :bar, baz: :qux}, {foo: :bar, baz: :boo}).should == [{ op: :replace, path: "/baz", value: :boo}]
+        subject.generate({foo: :bar, baz: :qux},
+                         {foo: :bar, baz: :boo})
+          .should == [{ op: :replace, path: "/baz", value: :boo}]
       end
 
       it "on array element" do
-        subject.generate({foo: [:bar, :qux, :baz]}, {foo: [:bar, :foo, :baz]}).should == [{ op: :replace, path: "/foo/1", value: :foo }]
+        subject.generate({foo: [:bar, :qux, :baz]},
+                         {foo: [:bar, :foo, :baz]})
+          .should == [{ op: :replace, path: "/foo/1", value: :foo }]
       end
 
       it "when type differ from array to hash" do
-        subject.generate({foo: [:bar]}, {foo: {bar: :foo}}).should == [{ op: :replace, path: "/foo", value: {bar: :foo} }]
+        subject.generate({foo: [:bar]},
+                         {foo: {bar: :foo}})
+          .should == [{ op: :replace, path: "/foo", value: {bar: :foo} }]
       end
 
       it "when type differ from hash to array" do
-        subject.generate({foo: {bar: :foo}}, {foo: [:bar]}).should == [{ op: :replace, path: "/foo", value: [:bar] }]
+        subject.generate({foo: {bar: :foo}},
+                         {foo: [:bar]})
+          .should == [{ op: :replace, path: "/foo", value: [:bar] }]
       end
     end
   end
